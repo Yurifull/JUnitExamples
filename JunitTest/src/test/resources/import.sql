84c89c84e71248cda6145523081e4d154e227a11
yperez <yperez@previred.com>
2019-09-12 09:53:51 -0300

    procedure

--- JunitTest/src/test/resources/import.sql
+++ JunitTest/src/test/resources/import.sql
@@ -12,3 +12,8 @@ INSERT INTO films VALUES ('llego');
 INSERT INTO films VALUES ('llego');
 INSERT INTO films VALUES ('llego');
 
+CREATE OR REPLACE FUNCTION findAllMovies()
+RETURNS setof films language sql as $$
+    select *
+    from films;
+$$;
