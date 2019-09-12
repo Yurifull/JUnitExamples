ed26339a0169ae5ddc461737d678b0ef555409ad
Yurifull <yurifull.evolution@gmail.com>
2019-09-12 12:18:36 -0300

    script finalies

--- JunitTest/src/test/resources/import.sql
+++ JunitTest/src/test/resources/import.sql
@@ -1,22 +1,35 @@
-CREATE TABLE films (code char(5));
+-- FUNCTION: junit.findallmovies()
+
+-- DROP FUNCTION junit.findallmovies();
+
+CREATE OR REPLACE FUNCTION junit.findallmovies(
+	)
+    RETURNS SETOF character varying 
+    LANGUAGE 'sql'
+
+    COST 100
+    VOLATILE 
+    ROWS 1000
+AS $BODY$SELECT code as code FROM junit.films;$BODY$;
+
+ALTER FUNCTION junit.findallmovies()
+    OWNER TO test;
+    
+    
+CREATE TABLE junit.films (code varchar(100));
 
 
-INSERT INTO films VALUES ('llego');
-INSERT INTO films VALUES ('llego');
-INSERT INTO films VALUES ('llego');
-INSERT INTO films VALUES ('llego');
-INSERT INTO films VALUES ('llego');
-INSERT INTO films VALUES ('llego');
-INSERT INTO films VALUES ('llego');
-INSERT INTO films VALUES ('llego');
-INSERT INTO films VALUES ('llego');
-INSERT INTO films VALUES ('llego');
+INSERT INTO junit.films VALUES ('Bob Esponja');
+INSERT INTO junit.films VALUES ('Transformers');
+INSERT INTO junit.films VALUES ('Pablo el Escoba');
+INSERT INTO junit.films VALUES ('Rapido y Furioso 20');
+INSERT INTO junit.films VALUES ('Condorito');
+INSERT INTO junit.films VALUES ('Lo que callan los Devs');
+INSERT INTO junit.films VALUES ('El Zorro');
+INSERT INTO junit.films VALUES ('Lego');
+INSERT INTO junit.films VALUES ('Titanic');
+INSERT INTO junit.films VALUES ('Regreso al futuro');
 
-CREATE OR REPLACE FUNCTION findAllMovies() RETURNS refcursor AS $$
-    DECLARE
-      ref refcursor;                                                     -- Declare a cursor variable
-    BEGIN
-      OPEN ref FOR SELECT code FROM films;   -- Open a cursor
-      RETURN ref;                                                       -- Return the cursor to the caller
-    END;
-    $$ LANGUAGE plpgsql;
+select junit.findallmovies();
+
+    
